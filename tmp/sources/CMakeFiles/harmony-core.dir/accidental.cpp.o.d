3968daca653d518ed34ff2a2e2887509bcbd382d
TimmMuranov <muranovtim@gmail.com>
2025-06-30 23:38:49 +0300

    Удалил папку симейка

--- tmp/sources/CMakeFiles/harmony-core.dir/accidental.cpp.o.d
+++
@@ -1,4 +0,0 @@
-sources/CMakeFiles/harmony-core.dir/accidental.cpp.o: \
- /home/tima/gitForks/harmony-core/sources/accidental.cpp \
- /usr/include/stdc-predef.h \
- /home/tima/gitForks/harmony-core/include/harmony-core/accidental.hpp
