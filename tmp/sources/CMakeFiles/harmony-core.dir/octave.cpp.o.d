5bad5914d83f49bed0518e8f302448acc8041e05
TimmMuranov <muranovtim@gmail.com>
2025-07-03 22:09:29 +0300

    Вечло забываю удалить билд директорию...

--- tmp/sources/CMakeFiles/harmony-core.dir/octave.cpp.o.d
+++
@@ -1,4 +0,0 @@
-sources/CMakeFiles/harmony-core.dir/octave.cpp.o: \
- /home/tima/gitForks/harmony-core/sources/octave.cpp \
- /usr/include/stdc-predef.h \
- /home/tima/gitForks/harmony-core/include/harmony-core/octave.hpp
