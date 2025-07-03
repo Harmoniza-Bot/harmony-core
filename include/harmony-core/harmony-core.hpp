2fc36d348a474ef889c53d0be7962a8ce404763b
TimmMuranov <muranovtim@gmail.com>
2025-07-03 23:45:20 +0300

    Добавил файл, объявляющий все сущности либы

---
+++ include/harmony-core/harmony-core.hpp
@@ -0,0 +1,9 @@
+#ifndef HARMONY_CORE_INCLUDES
+#define HARMONY_CORE_INCLUDES
+
+#include "accidental.hpp"
+#include "key.hpp"
+#include "note.hpp"
+#include "octave.hpp"
+
+#endif // HARMONY_CORE_INCLUDES
