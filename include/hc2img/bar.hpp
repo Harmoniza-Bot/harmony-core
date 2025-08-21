db47dde6f8b3c769eba4825da6b55c507d4914f5
TimmMuranov <muranovtim@gmail.com>
2025-08-21 20:44:03 +0300

    Добавил оставшиеся функции в такт. Необходимо реализовать.

--- include/hc2img/bar.hpp
+++ include/hc2img/bar.hpp
@@ -15,8 +15,8 @@ namespace hc2img {
      * - о ключе (определяется с помощью класса harmony_core::Clef).
      * - о ключевых знаках (определяется с помолью класса harmony_core::Key).
      * Также в такт можно добавить ноты и интервалы (клсcы harmony_core::Note и harmony_core::Interval).
-     * Возможность добавления ноты определяется размером такта, размером ноты и суммой размеров нот в такте.
-     * (Для функционирования интерфейса необходимо подключить библиотеку CImg.h)
+     * Интервал хранится как две ноты в одинаковых индексах.
+     * Возможность добавления определяется размером такта, размером ноты и суммой размеров нот в такте.
      */
     struct Bar final {
       public:
@@ -24,15 +24,48 @@ namespace hc2img {
          * \brief Добавляет ноту, если она помещается в такт.
          * Пустые места в такте отображаются как паузы.
          * \param [in] harmony_core::Note добавляемая нота.
+         * \param [in] uint8_t Индекс ноты.
          */
-        void add(harmony_core::Note &) noexcept;
+        void add(harmony_core::Note &, uint8_t) noexcept;
         
         /*!
          * \brief Убирает ноту(ы) по индексу.
          * На месте удаленного объекта образуется пустота, отображаемая паузой при отрисовке такта.
          * \param [in] uint8_t Индекс ноты для удаления.
          */
-        void rm(uint8_t) noexcept;
+        void remove(uint8_t) noexcept;
+
+        /*!
+         * Возвращает ноту, расположенную по индексу.
+         * \param [in] uint8_t Индекс ноты.
+         * \return Нота по индексу.
+         */
+        harmony_core::Note get(uint8_t) const noexcept;
+
+        /*!
+         * \brief Задает или переписывает размер такта.
+         * \param [in] uint8_t Числитель размера
+         * \param [in] uint8_t Знаменатель размера
+         */
+        void set_time_signature(uint8_t, uint8_t) noexcept;
+
+        /*!
+         * Возвращает текущий размер такта.
+         * \return размер такта.
+         */
+        std::pair<uint8_t, uint8_t> get_time_signature() const noexcept;
+
+        /*!
+         * Задает или переписывает ключевые знаки (тональность) такта.
+         * \param [in] harmony_core::Key Тональность для замены/установки значения.
+         */
+        void set_key(harmony_core::Key) noexcept;
+
+        /*!
+         * Возвращает тональность такта
+         * \return Тональность такта.
+         */
+        harmony_core::Key get_key() const noexcept;
 
     private:
         /*!
