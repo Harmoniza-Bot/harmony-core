540cc64fd3801d8a798bda5df8f2af9d069b10c7
TimmMuranov <muranovtim@gmail.com>
2025-09-02 14:08:57 +0300

    Добавил определение методов размера. нужно реализовать

--- include/harmony-core/time_signature.hpp
+++ include/harmony-core/time_signature.hpp
@@ -3,6 +3,8 @@
 
 #include <cstdint>
 
+#include "harmony_core/note.hpp"
+
 namespace harmony_core {
     /*!
      * \brief Данный класс представляет музыкальный размер.
@@ -11,7 +13,45 @@ namespace harmony_core {
      */
     class Time_signature final {
     public:
-        // vrite getters & setters
+        /*!
+         * \brief Создает размер 4/4.
+         */
+        Time_signature();
+        
+        /*!
+         * \brief Создает размер по числителю и знаменателю.
+         */
+         Time_signature(uint8_t, uint8_t);
+         
+        /*!
+         * \brief Создает размер на основе другого размера.
+         */
+        Time_signature(Time_signature);
+        
+        // getters & setters
+        
+        /*!
+         * \brief Задает числитель и знаменатель.
+         */
+        void set_signature(uint8_t, uint8_t) noexcept;
+        
+        /*!
+         * \brief Возвращает числитель размера.
+         */
+        uint8_t get_numerator() const noexcept;
+        
+        /*!
+         * \brief Возвращает знаменатель размера.
+         */
+        uint8_t get_denominator() const noexcept;
+        
+        /*!
+         * \brief Возвращает остаточный размер такта после добавления списка нот или -1, если ноты не помещаются.
+         * \param [in] список нот для добавления
+         * \return Пару числитель - знаменатель остатка размера.
+        */
+        std::pair<uint8_t, uint8_t> get_remainder(std::vector<harmony_core::Note>);
+        
     private:
         /*!
          * Представляет информацию о размере
