89b71d3a01f699e6f5f0df27c226528eed074d0f
TimmMuranov <muranovtim@gmail.com>
2025-09-03 13:17:43 +0300

    написал пустышки реализации функций нотного стана

--- sources/staff.cpp
+++ sources/staff.cpp
@@ -1,3 +1,20 @@
 #include <hc2img/staff.hpp>
 
 using namespace hc2img;
+
+Staff::Staff(){
+    
+}
+
+void Staff::add(Note *n, bool is_pause) noexcept {
+    
+}
+
+void Staff::rm(uint16_t) noexcept {
+    
+}
+
+size_t Staff::get_size() const noexcept {
+    
+}
+
