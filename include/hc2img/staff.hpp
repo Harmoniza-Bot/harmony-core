b56b65a24f66b1a4daef88d38c937d4c820ae4a5
TimmMuranov <muranovtim@gmail.com>
2025-09-04 11:50:55 +0300

    добавил геттеры и сеттеры в стан. Добавил инкремент/декремент. Редактировал документацию

--- include/hc2img/staff.hpp
+++ include/hc2img/staff.hpp
@@ -15,7 +15,46 @@ namespace hc2img {
         /*!
          * \brief создает нотный стан без нот со скрипичным ключем в размере 4/4.
          */
-        Staff();
+        explicit Staff();
+        
+        /*!
+         * \brief создает нотный стан на основе днугого нотного стана.
+         */
+         explicit Staff(hc2img::Staff);
+         
+        /*!
+         * \brief устанавливает ключ.
+         * \param [in] harmony_core::Clef
+         */
+         void set_clef(harmony_core::Clef) noexcept;
+         
+         /*!
+          * \brief возвращает ключ.
+          * \return ключ стана.
+          */ 
+          harmony_core::Clef get_clef() noexcept;
+          
+         /*!
+         * \brief устанавливает размер нотного стана.
+         * \param [in] harmony_core::Time_signature
+         */
+         void set_time_signature(harmony_core::Time_signature) noexcept;
+         
+         /*!
+          * \brief возвращает музыкальный размер стана.
+          * \return размер стана (один на весь стан).
+          */
+          harmony_core::time_signature get_time_signature() noexcept;
+          
+         /*!
+          * \brief возвращает ноту по индексу.
+          */
+         std::pair<Note::Note n, bool i> get_note(uint16_t i);
+         
+        /*!
+         * \brief возвращает количество нот и пауз в нотном стане.
+         */
+         size_t get_note_list_size();
         
         /*!
          * \brief Добавляет ноту в такт.
@@ -32,9 +71,9 @@ namespace hc2img {
         void rm(uint16_t index) noexcept;
 
         /*!
-         * \brief Возвращает количество целых долей в нотном стане.
+         * \brief Возвращает количество тактов в  нотном стане.
          */
-        size_t get_whole() const noexcept;
+        size_t get_size() const noexcept;
         
         /*!
          * \brief инкрементирует указатель на ноту стана.
--- sources/staff.cpp
+++ sources/staff.cpp
@@ -2,11 +2,44 @@
 #include <cmath>
 using namespace hc2img;
 
-Staff::Staff(){
+explicit Staff::Staff(){
     harmony_core::Clef c;
     clef = c;
     harmony_core::Time_signature t_s;
     time_sig = t_s;
+}
+
+explicit Staff::Staff(hc2img::Staff s){
+    clef = s.get_clef();
+    time_sig = s.get_time_signature();
+    ptr_index = 0;
+    for(int x=0; x<s.get_note_list_size(); ++x){
+        note_list[x] = s.get_note(x);
+    }
+}
+
+void set_clef(harmony_core::Clef c){
+    clef = *c.get_clef();
+}
+
+harmony_core::Clef get_clef(){
+    return *clef;
+}
+
+void set_time_signature(harmony_core::Time_signature t){
+    time_sig = *t.get_time_signature();
+}
+        
+harmony_core::time_signature get_time_signature(){
+    return *time_sig;
+}
+
+std::pair<Note::Note n, bool i> get_note(uint16_t i){
+    return note_list[i];
+}
+
+size_t get_note_list_size(){
+    return note_list.size();
 }
 
 void Staff::add(harmony_core::Note *n, bool is_pause) noexcept {
@@ -18,11 +51,14 @@ void Staff::add(harmony_core::Note *n, bool is_pause) noexcept {
     }
 }
 
-void Staff::rm(uint16_t) noexcept {
-    
+void Staff::rm(uint16_t index) noexcept {
+    if(index + 1 > note_list.size()){
+        return;
+    }
+    note_list.erase(note_list.begin() + index);
 }
 
-size_t Staff::get_whole() const noexcept {
+size_t Staff::get_size() const noexcept {
     float full_note_size = 0;
     for(int x=0; x<note_list.size(); ++x){
         static uint8_t note_size;
@@ -49,10 +85,22 @@ Staff &Staff::operator++(){
     return *this;
 }
 
+Staff Staff::operator++(int){
+    Staff s(*this);
+    ++*this;
+    return s;
+}
+
 Staff &Staff::operator--(){
     if(index == 0){
         return *this;
     }
     --ptr_index;
     return *this;
+}
+
+Staff Staff::operator--(int){
+    Staff s(*this);
+    --*this;
+    return s;
 }
