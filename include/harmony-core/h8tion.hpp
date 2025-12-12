fa82cc0180db582cccf9c2db008b55e74e28e121
TimmMuranov <muranovtim@gmail.com>
2025-12-12 21:39:16 +0300

    Добавил модуль гармонизации. Если зайдёт - оставлю, иначе всё инкапсулирую в сами объекты.

---
+++ include/harmony-core/h8tion.hpp
@@ -0,0 +1,6 @@
+#ifndef H8TION_EXTERN_INCLUDE
+#define H8TION_EXTERN_INCLUDE
+
+#include h8tion/h8tion.hpp
+
+#endif //H8TION_EXTERN_INCLUDE
---
+++ include/harmony-core/h8tion/h8tion.hpp
@@ -0,0 +1,11 @@
+#ifndef H8TION_INTERNAL_INCLUDE
+#define H8TION_INTERNAL_INCLUDE
+
+/*! 
+ * Данное пространство имен хранит функции, ответственные за 
+ * сложные манипуляции с нотами, интервалвми и аккордами.
+ */
+namespace h8tion{
+}
+
+#endif //H8TION_INTERNAL_INCLUDE
