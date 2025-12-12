dd5de7a3391f5294fc5ede74d17c75a67d1bef0b
TimmMuranov <muranovtim@gmail.com>
2025-12-12 21:29:11 +0300

    Добавил описания оставшихся неймспейсов. Начинаю работать над модулем гармонизации.

--- include/harmony-core/hc2img/hc2img.hpp
+++ include/harmony-core/hc2img/hc2img.hpp
@@ -1,6 +1,14 @@
 #ifndef HC2IMG_INCLUDES
 #define HC2IMG_INCLUDES
 
+/*! 
+ * Данное пространство имен хранит объекты,
+ * необходимве для отображения структур harmony_core.
+ * Доступно создание листа и добавление на него нотного стана.
+ */
+  namespace hc2img{
+ }
+
 #include "list.hpp"
 #include "list_param.hpp"
 #include "staff_cord.hpp"
